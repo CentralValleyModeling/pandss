b773fb19aab0ab9b8628bdd2b2a9edb1326a15a2
Zachary Roy <zachary.roy@water.ca.gov>
2023-06-16 08:42:25 -0700

    Change docstring format

--- pandss/read.py
+++ pandss/read.py
@@ -12,9 +12,9 @@ CONTEXT_ATTR = (
 )
 
 
-def read_ledger(
+def read_dss(
         dss: Union[Path, str], 
-        paths: Iterable[str], 
+        paths: Union[Iterable[str], pd.DataFrame], 
         add_context: Union[bool, Iterable[str]]
     ) -> pd.DataFrame:
     """Create a DataFrame ledger from a set of Timeseries in a DSS file. 
@@ -23,23 +23,32 @@ def read_ledger(
     the timeseries units, and the indexing period type can be added with the 
     optional `add_context
 
-    Args:
-        dss (Union[Path | str]): The filepath of the DSS file
-        paths (Iterable[str]): A list of DSS A-F paths to be read
-        add_context (Union[bool, Iterable[str]]): An iterable like ['PATH', 'UNITS', 'PERIOD_TYPE'], or a subset of these.
+    Parameters
+    ----------
+    dss : Union[Path, str]
+        The filepath of the DSS file.
+    paths : Union[Iterable[str], pd.DataFrame]
+        A list of DSS A-F paths to be read, or a catalog DataFrame.
+    add_context : Union[bool, Iterable[str]]
+        An iterable like ['PATH', 'UNITS', 'PERIOD_TYPE'], or a subset of these.
 
-    Returns:
-        pandas.DataFrame: A long-list DataFrame of the timeseries
-    """
+    Returns
+    -------
+    pd.DataFrame
+        A long-list DataFrame of the timeseries
+    """    
     if add_context is True:  # Add all
         add_context = CONTEXT_ATTR
     elif add_context is False:  # Add none
         add_context = tuple()
     else:  # Make sure they are all recognizable
         add_context = [i.upper() for i in add_context]
-        assert all([i for i in add_context in CONTEXT_ATTR]) 
+        assert all([i for i in add_context in CONTEXT_ATTR])     
 
     with pyhecdss.DSSFile(str(dss)) as DSS:
+        if isinstance(paths, pd.DataFrame):  # Resolve the catalog to pathnames
+            paths = DSS.get_pathnames(paths)
+
         frames = list()
         for p in paths:
             ts = DSS.read_rts(p)
--- pandss/reshape.py
+++ pandss/reshape.py
@@ -1,3 +1,5 @@
+from typing import Union, Iterable
+
 import pandas as pd
 
 
@@ -11,15 +13,21 @@ PART_NAMES = {
 }
 
 
-def split_path_column(df: pd.DataFrame, semantic: bool = False) -> pd.DataFrame:
+def split_path_column(df: pd.DataFrame, semantic: Union[bool, Iterable] = False) -> pd.DataFrame:
     """Split the PATH column in a DataFrame into it's components. 
 
-    Args:
-        df (pd.DataFrame): A DataFrame with a HEC-DSS style path column.
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        A pandas DataFrame with a HEC-DSS style path column.
+    semantic : Union[bool, Iterable], optional
+        Change the A-F column labels, True gives CalSim3 interpretation, by default False
 
-    Returns:
-        pd.DataFrame: A copy of the original DataFrame with 6 new columns.
-    """
+    Returns
+    -------
+    pandas.DataFrame
+        A copy of the original pandas DataFrame with 6 new columns.
+    """    
     df = df.copy()
     df[['A', 'B', 'C', 'D', 'E', 'F']] = df['PATH']\
         .str.strip('/')\
