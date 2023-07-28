83294e093c65668fcb564464ce7030e50ab272a3
Zachary Roy <zachary.roy@water.ca.gov>
2023-07-28 13:29:03 -0700

    Add tests for common catalog function

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -22,5 +22,34 @@ class TestCatalog(unittest.TestCase):
         average = sum(times) / len(times)
         self.assertLessEqual(average, 0.01)
 
+class TestCommonCatalog(unittest.TestCase):
+    def test_ignore_all_raises(self):
+        with self.assertRaises(ValueError):
+            pdss.common_catalog(
+                left=DSS_SMALL, 
+                right=DSS_SMALL, 
+                ignore_parts=pdss.catalog.COMPARE_ON)
+    
+    def test_with_duplicate(self):
+        cat_1 = DataFrame(
+            [['T', 'A1', 'B', 'C', 'D', 'E'],
+            ['T', 'A2', 'B', 'C', 'D', 'E'],
+            ['T', 'A2', 'B', 'C', 'D', 'E']],
+            columns=['T', 'A', 'B', 'C', 'D', 'E']
+        )
+        cat_2 = DataFrame(
+            [['T', 'A1', 'B', 'C', 'D', 'E'],
+            ['T', 'A2', 'B', 'C', 'D', 'E'],
+            ['T', 'A2', 'B', 'C', 'D', 'E'],
+            ['T', 'A3', 'B', 'C', 'D', 'E']],
+            columns=['T', 'A', 'B', 'C', 'D', 'E']
+        )
+        with self.assertWarns(Warning):
+            L, R =pdss.common_catalog(
+                cat_1, 
+                cat_2, 
+                ignore_parts=['B', 'C', 'D', 'E'])
+        self.assertEqual(len(L), len(R))
+            
 if __name__ == '__main__':
     unittest.main()
