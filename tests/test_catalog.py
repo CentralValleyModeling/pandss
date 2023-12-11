c178e21b6e1823cc6236b81c9fff0e0b109d6e10
Zachary Roy <zachary.roy@water.ca.gov>
2023-12-11 13:35:38 -0800

    Run black and isort

--- tests/test_catalog.py
+++ tests/test_catalog.py
@@ -3,18 +3,20 @@ from pathlib import Path
 from time import perf_counter
 
 from pandas import DataFrame, Series
+
 import pandss as pdss
 
-DSS_SMALL = Path('./assets/small.dss').resolve()
+DSS_SMALL = Path("./assets/small.dss").resolve()
+
 
 class TestCatalog(unittest.TestCase):
     def test_read_type(self):
         catalog = pdss.read_catalog(DSS_SMALL)
         self.assertIsInstance(catalog, DataFrame)
-    
+
     def test_read_time(self):
         times = list()
-        for _ in range(100): 
+        for _ in range(100):
             st = perf_counter()
             _ = pdss.read_catalog(DSS_SMALL)
             et = perf_counter()
@@ -22,94 +24,94 @@ class TestCatalog(unittest.TestCase):
         average = sum(times) / len(times)
         self.assertLessEqual(average, 0.01)
 
+
 class TestCommonCatalog(unittest.TestCase):
     def test_ignore_all_raises(self):
         with self.assertRaises(ValueError):
             pdss.common_catalog(
-                left=DSS_SMALL, 
-                right=DSS_SMALL, 
-                ignore_parts=pdss.catalog.COMPARE_ON)
-    
+                left=DSS_SMALL, right=DSS_SMALL, ignore_parts=pdss.catalog.COMPARE_ON
+            )
+
     def test_with_duplicate(self):
         cat_1 = DataFrame(
-            [['T', 'A1', 'B', 'C', 'D', 'E'],
-            ['T', 'A2', 'B', 'C', 'D', 'E'],
-            ['T', 'A2', 'B', 'C', 'D', 'E']],
-            columns=['T', 'A', 'B', 'C', 'D', 'E']
+            [
+                ["T", "A1", "B", "C", "D", "E"],
+                ["T", "A2", "B", "C", "D", "E"],
+                ["T", "A2", "B", "C", "D", "E"],
+            ],
+            columns=["T", "A", "B", "C", "D", "E"],
         )
         cat_2 = DataFrame(
-            [['T', 'A1', 'B', 'C', 'D', 'E'],
-            ['T', 'A2', 'B', 'C', 'D', 'E'],
-            ['T', 'A2', 'B', 'C', 'D', 'E'],
-            ['T', 'A3', 'B', 'C', 'D', 'E']],
-            columns=['T', 'A', 'B', 'C', 'D', 'E']
+            [
+                ["T", "A1", "B", "C", "D", "E"],
+                ["T", "A2", "B", "C", "D", "E"],
+                ["T", "A2", "B", "C", "D", "E"],
+                ["T", "A3", "B", "C", "D", "E"],
+            ],
+            columns=["T", "A", "B", "C", "D", "E"],
         )
         with self.assertWarns(Warning):
-            L, R =pdss.common_catalog(
-                cat_1, 
-                cat_2, 
-                ignore_parts=['B', 'C', 'D', 'E'])
+            L, R = pdss.common_catalog(cat_1, cat_2, ignore_parts=["B", "C", "D", "E"])
         self.assertEqual(len(L), len(R))
+
 
 class TestCatalogObject(unittest.TestCase):
     def test_creation(self):
         cat = pdss.Catalog(
             data=[
-                ['T', 'A1', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F']
+                ["T", "A1", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
             ],
-            columns=['T', 'A', 'B', 'C', 'D', 'E', 'F']
+            columns=["T", "A", "B", "C", "D", "E", "F"],
         )
         self.assertIsInstance(cat, pdss.Catalog)
-    
+
     def test_error_on_missing_column(self):
         with self.assertRaises(ValueError):
             cat = pdss.Catalog(
                 data=[
-                    ['A1', 'B', 'C', 'E'],
-                    ['A2', 'B', 'C', 'E'],
-                    ['A2', 'B', 'C', 'E']
+                    ["A1", "B", "C", "E"],
+                    ["A2", "B", "C", "E"],
+                    ["A2", "B", "C", "E"],
                 ],
-                columns=['A', 'B', 'C', 'E']
+                columns=["A", "B", "C", "E"],
             )
-    
+
     def test_error_on_extra_column(self):
         with self.assertRaises(ValueError):
             cat = pdss.Catalog(
                 data=[
-                ['T', 'A1', 'B', 'C', 'D', 'E', 'F', 'G'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F', 'G'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F', 'G']
-            ],
-            columns=['T', 'A', 'B', 'C', 'D', 'E', 'F', 'G']
+                    ["T", "A1", "B", "C", "D", "E", "F", "G"],
+                    ["T", "A2", "B", "C", "D", "E", "F", "G"],
+                    ["T", "A2", "B", "C", "D", "E", "F", "G"],
+                ],
+                columns=["T", "A", "B", "C", "D", "E", "F", "G"],
             )
-    
+
     def test_slice_type(self):
         cat = pdss.Catalog(
             data=[
-                ['T', 'A1', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F']
+                ["T", "A1", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
             ],
-            columns=['T', 'A', 'B', 'C', 'D', 'E', 'F']
+            columns=["T", "A", "B", "C", "D", "E", "F"],
         )
-        self.assertIsInstance(cat['A'], Series)
-    
+        self.assertIsInstance(cat["A"], Series)
+
     def test_metadata(self):
         cat = pdss.Catalog(
             data=[
-                ['T', 'A1', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F'],
-                ['T', 'A2', 'B', 'C', 'D', 'E', 'F']
+                ["T", "A1", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
+                ["T", "A2", "B", "C", "D", "E", "F"],
             ],
-            columns=['T', 'A', 'B', 'C', 'D', 'E', 'F']
+            columns=["T", "A", "B", "C", "D", "E", "F"],
         )
         cat = cat.copy()
-        self.assertTrue(hasattr(cat, 'source'))
+        self.assertTrue(hasattr(cat, "source"))
 
-        
 
-            
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- tests/test_heclib.py
+++ tests/test_heclib.py
@@ -1,17 +1,106 @@
-import logging
+import ctypes as ct
+import unittest
 from datetime import datetime
 
-from pandss import HECLIB
+import numpy as np
+
+from pandss import DSS_Handle, heclib
 
 
-if __name__ == '__main__':
-    #logging.basicConfig(level=logging.DEBUG)
-    dss_file = "C:/Users/zroy/Documents/_Python/pandss/tests/test.dss"
-    with HECLIB(dss_file) as DLL:
-        paths, record_types = DLL.catalog()
-        result = DLL.ts_retrieve(
-            path=paths[0], 
-            start=datetime(1920, 1, 1, 0, 0),
-            end=datetime(1929, 12, 31, 23, 59)
-        )
-        
+class Test_DSS_Handle(unittest.TestCase):
+    @classmethod
+    def setUp(cls):
+        cls.dss_file = "C:/Users/zroy/Documents/_Python/pandss/tests/test.dss"
+
+    def _test_all(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            # paths, record_types = DSS.catalog()
+            # result = DSS.ts_info(paths[0])
+            # result = DSS.ts_retrieve(paths[0])
+            # result = DSS.ret()
+            new_path = "/PANDSS/NEW_TS_2/TESTING//1MON/L2023A"
+            units = "HOPE"
+            period_type = "PER-AVER"
+            values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            quality = []
+            result = DSS.ts_store_regular(
+                new_path,
+                date_range=[datetime(2023, 1, 1), datetime(2023, 12, 1)],
+                values=values,
+                quality=quality,
+                units=units,
+                period_type=period_type,
+            )
+
+            print(result)
+
+    def test_open_close(self):
+        try:
+            with DSS_Handle(self.dss_file) as DSS:
+                pass
+        except Exception as e:
+            self.fail()
+
+    def test_catalog(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            paths, record_types = DSS.catalog()
+        self.assertIsInstance(paths, np.ndarray)
+        self.assertIsInstance(paths, np.ndarray)
+
+    def test_ts_info(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            paths, record_types = DSS.catalog()
+            units, period_type = DSS.ts_info(paths[0])
+        self.assertIsInstance(units, str)
+        self.assertIsInstance(period_type, str)
+
+    def test_get_datetime_range(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            paths, record_types = DSS.catalog()
+            sj, ss, ej, es = DSS._get_datetime_range(paths[0].encode())
+        self.assertIsInstance(sj, ct.c_long)
+        self.assertIsInstance(ss, ct.c_long)
+        self.assertIsInstance(ej, ct.c_long)
+        self.assertIsInstance(es, ct.c_long)
+
+    def test_date_range_to_datetime(self):
+        sj = ct.c_long(7974)
+        ss = ct.c_long(86400)
+        ej = ct.c_long(10957)
+        es = ct.c_long(86400)
+        start, stop = heclib.dates.get_datetime_range_pyobj(sj, ss, ej, es)
+        self.assertIsInstance(start, datetime)
+        self.assertIsInstance(stop, datetime)
+        self.assertEqual(start, datetime(1921, 11, 1))
+
+    def test_get_ts_sizes(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            paths, record_types = DSS.catalog()
+            path_bytes = paths[0].encode()
+            date_range_julian = DSS._get_datetime_range(path_bytes)
+            start, end = heclib.dates.get_datetime_range_pyobj(*date_range_julian)
+            start_date, start_time = heclib.dates.datetime_encode(start)
+            end_date, end_time = heclib.dates.datetime_encode(end)
+            time_len, quality_len = DSS._get_ts_sizes(
+                path_bytes,
+                start_date,
+                start_time,
+                end_date,
+                end_time,
+            )
+        self.assertIsInstance(time_len, ct.c_long)
+        self.assertIsInstance(quality_len, ct.c_long)
+
+    def test_ts_retrieve(self):
+        with DSS_Handle(self.dss_file) as DSS:
+            paths, record_types = DSS.catalog()
+
+            values, dates = DSS.ts_retrieve(paths[0], full_set=True)
+
+        self.assertIsInstance(values, np.ndarray)
+        self.assertIsInstance(dates, list)
+        self.assertEqual(len(values), 1200)
+
+
+if __name__ == "__main__":
+    unittest.main()
