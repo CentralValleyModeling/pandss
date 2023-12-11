f9a5ab545f2a7ddd514b2187fbb9d296a294cf6b
Zachary Roy <zachary.roy@water.ca.gov>
2023-12-11 14:29:56 -0800

    Add docstrings

--- pandss/heclib/dss_handle.py
+++ pandss/heclib/dss_handle.py
@@ -25,13 +25,28 @@ class DSS_Handle:
 
     @silent
     def __enter__(self):
+        """Wraps DLL function `hec_dss_open` and enables the use of this class
+        in pythons context manager pattern.
+
+        with DSS_Handle(path_to_dss_file) as DSS:
+            # read/write data in DSS file
+            ...
+        # hec_dss_close automatically called.
+
+
+        Returns
+        -------
+        self
+            Returns a DSS_Handle object.
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error opening DSS file
+        """
         logging.info(f"opening dss file {self.file_path}")
         fp = str(self.file_path).encode()
-        try:
-            success = self.dll.hec_dss_open(fp, ct.byref(self.file_table))
-        except Exception as e:
-            logging.error("exception caught before DLL function returned")
-            raise e
+        success = self.dll.hec_dss_open(fp, ct.byref(self.file_table))
         if success != 0:
             raise RuntimeError("")  # TODO create error handling
         self.open = True
@@ -41,6 +56,9 @@ class DSS_Handle:
     @silent
     @must_be_open
     def __exit__(self, exc_type, exc_inst, traceback):
+        """Wraps DLL function `hec_dss_close` and enables the use of this class
+        in pythons context manager pattern.
+        """
         logging.info(f"closing dss file {self.file_path}")
         success = self.dll.hec_dss_close(self.file_table)
         if success != 0:
@@ -50,6 +68,29 @@ class DSS_Handle:
     @silent
     @must_be_open
     def catalog(self, path_filter: str = "") -> tuple[np.ndarray, np.ndarray]:
+        """Wraps `hec_dss_catalog` and other DSS functions needed for reading
+        the DSS catalog.
+
+        with DSS_Handle(path) as DSS:
+            catalog, record_types = DSS.catalog()
+
+        Parameters
+        ----------
+        path_filter : str, optional
+            A str that represents a pathname with wild characters represented
+            by a star (*) to match any string in the pathname part. Wild
+            characters can be at the beginning or end of a part, not inside of
+            a string. As an example, "/*/*/*Flow/*/*/*/" will match all
+            pathnames that have "Flow" anywhere in the C part, such as "Flow",
+            "Inflow", "Outflow-Reg", etc. A part such as "Flow*Reg" is not
+            legal. A null (//) will only match a null, where only a star (*)
+            will match all., by default ""
+
+        Returns
+        -------
+        tuple[np.ndarray, np.ndarray]
+            char array of catalog paths, and an array of matching record types
+        """
         logging.info(f"getting catalog from dss {self.file_path}")
         path_filter = path_filter.encode("ascii")
         # Determine sizes of arrays
@@ -81,6 +122,24 @@ class DSS_Handle:
     @silent
     @must_be_open
     def ts_info(self, path: str) -> tuple[str, str]:
+        """Wraps `hec_dss_tsRetrieveInfo` and returns the units, period_type of
+        a timeseries.
+
+        Parameters
+        ----------
+        path : str
+            A-F path of a DSS timeseries
+
+        Returns
+        -------
+        tuple[str, str]
+            Values for `untis` and `period_type` of the timeseries
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error reading info from the DSS file
+        """
         logging.info(f"getting units and period_type for {path}")
         path_bytes = path.encode("UTF-8")
         buffer_size = ct.c_int(10)
@@ -99,13 +158,34 @@ class DSS_Handle:
 
         return units.value.decode(), period_type.value.decode()
 
-    # @silent
+    @silent
     @must_be_open
     def _get_datetime_range(
         self,
         path: bytes,
         full_set: bool = True,
     ) -> tuple[ct.c_int, ct.c_int, ct.c_int, ct.c_int]:
+        """Wraps hec_dss_tsGetDateTimeRange and returns the julian, seconds
+        start and end dates of the timeseries.
+
+        Parameters
+        ----------
+        path : str
+            A-F path to the timeseries
+        full_set : bool, optional
+            Corresponds to the boolFullSet flag, by default True
+
+        Returns
+        -------
+        tuple[ct.c_int, ct.c_int, ct.c_int, ct.c_int]
+            The start date in julian format, the start time in seconds, the end
+            date in julian format, the end time in seconds
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error reading info from the DSS file
+        """
         # read date span
         start_julian = ct.c_int()
         start_seconds = ct.c_int()
@@ -146,7 +226,34 @@ class DSS_Handle:
         start_time: ct.c_char_p,
         end_date: ct.c_char_p,
         end_time: ct.c_char_p,
-    ):
+    ) -> tuple[ct.c_int, ct.c_int]:
+        """Wraps hec_dss_tsGetSizes and returns the array length and quality
+        table width for the given path and date range.
+
+        Parameters
+        ----------
+        path : bytes
+            A-F path to the timeseries
+        start_date : ct.c_char_p
+            Inclusive start date in DDMMMYYYY format
+        start_time : ct.c_char_p
+            Inclusive start time in HHMM format
+        end_date : ct.c_char_p
+            Inclusive end date in DDMMYYY format
+        end_time : ct.c_char_p
+            Inclusive end date in HHMM format
+
+        Returns
+        -------
+        tuple[ct.c_int, ct.c_int]
+            The length and width of arrays needed to store the data in this timeseries
+            and associated quality data
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error reading info from the DSS file
+        """
         logging.debug("getting timeseries size")
         # Init
         time_len = ct.c_int()
@@ -171,7 +278,31 @@ class DSS_Handle:
 
     @silent
     @must_be_open
-    def ts_retrieve(self, path: str, full_set: bool = True):
+    def ts_retrieve(
+        self,
+        path: str,
+        full_set: bool = True,
+    ) -> tuple[np.ndarray, np.ndarray]:
+        """Wraps `hec_dss_tsRetrieve` and associated functions to read timeseries data
+        from a DSS file.
+
+        Parameters
+        ----------
+        path :
+            A-F path to the timeseries
+        full_set : bool, optional
+            Corresponds to the boolFullSet flag, by default True, by default True
+
+        Returns
+        -------
+        tuple[np.ndarray, np.ndarray]
+            The values and and times read for the timeseries
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error reading info from the DSS file
+        """
         path_bytes = path.encode("UTF-8")
         # Determine date range
         date_range_julian = self._get_datetime_range(path_bytes, full_set)
@@ -237,6 +368,8 @@ class DSS_Handle:
 
         return values, times
 
+    @silent
+    @must_be_open
     def ts_store_regular(
         self,
         path: str,
@@ -246,7 +379,35 @@ class DSS_Handle:
         units: str,
         period_type: str,
         save_as_float: bool = True,
-    ):
+    ) -> None:
+        """Wraps `hec_dss_tsStoreRegular`.
+
+        Parameters
+        ----------
+        path : str
+            A-F path for the timeseries provided.
+        date_range : tuple[datetime, datetime]
+            The inclusive datetime range for the timeseries
+        values : np.ndarray
+            The array of values to be saved
+        quality : np.ndarray
+            The table of quality data to be saved
+        units : str
+            The units for the timeseries
+        period_type : str
+            The period_type for the timeseries
+        save_as_float : bool, optional
+            Corresponds to the saveAsFloat flag, by default True
+
+        Returns
+        -------
+        None
+
+        Raises
+        ------
+        RuntimeError
+            Generic error for error reading info from the DSS file
+        """
         path_bytes = path.encode("UTF-8")
         start_date, start_time = datetime_encode(date_range[0])
         end_date, end_time = datetime_encode(date_range[1])
@@ -275,8 +436,4 @@ class DSS_Handle:
         )
 
         if success != 0:
-            print(success)
-            print(self.file_table)
             raise RuntimeError("")  # TODO create error handling
-
-        return success
