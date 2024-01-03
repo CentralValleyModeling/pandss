c5b2a71e83ba2df608871f6a211a0f3c162cd434
Zachary Roy <zachary.roy@water.ca.gov>
2024-01-03 15:17:26 -0800

    Rearrange imports

--- pandss/__init__.py
+++ pandss/__init__.py
@@ -3,11 +3,11 @@
 Zachary Roy, DWR
 June 2023
 """
+from . import keywords
 from .catalog import Catalog
 from .dss import DSS
 from .paths import DatasetPath
 from .timeseries import RegularTimeseries
 from .utils import read_catalog, read_rts
-from . import keywords
 
 __all__ = ["read_catalog", "read_rts"]
