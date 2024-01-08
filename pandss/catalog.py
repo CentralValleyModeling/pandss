14616a8407ff8e4bb5efe9d89922f3c90630c6ea
Zachary Roy <zachary.roy@water.ca.gov>
2024-01-08 11:52:08 -0800

    Add from_frame method to Catalog object

--- pandss/catalog.py
+++ pandss/catalog.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Self
 
+from pandas import DataFrame
+
 from .paths import DatasetPath, DatasetPathCollection
 
 
@@ -23,6 +25,20 @@ class Catalog(DatasetPathCollection):
             paths=paths,
             src=src,
         )
+
+    @classmethod
+    def from_frame(cls, df: DataFrame, src: Path) -> Self:
+        df.columns = df.columns.str.lower()
+        missing = [c for c in ("a", "b", "c", "d", "e", "f") if c not in df.columns]
+        if missing:
+            raise ValueError(
+                f"DataFrame is misssing required columns: {missing}\n\t{df.sample(2)}"
+            )
+        df = df[["a", "b", "c", "d", "e", "f"]]
+        paths = set(DatasetPath(*row) for row in df.itertuples(index=False))
+        if any(p.has_wildcard for p in paths):
+            raise ValueError(f"{cls.__name__} cannot be created with wildcard paths")
+        return cls(paths=paths, src=src)
 
     def findall(self, path: DatasetPath) -> DatasetPathCollection:
         return super(Catalog, self).findall(path)
