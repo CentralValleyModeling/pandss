e3ae8cfa8f77ad760fd5208e3b8abbe955793722
Zachary Roy <zachary.roy@water.ca.gov>
2024-01-08 15:39:51 -0800

    Move object construction logic to engines

--- pandss/timeseries.py
+++ pandss/timeseries.py
@@ -1,9 +1,8 @@
 from dataclasses import dataclass, fields
-from datetime import datetime, time, timedelta
-from typing import Any, Self
 
-import numpy as np
-from pandas import DataFrame, MultiIndex
+from numpy import datetime64, float64
+from numpy.typing import NDArray
+from pandas import DataFrame, DatetimeIndex, MultiIndex, PeriodIndex
 
 from .paths import DatasetPath
 
@@ -16,8 +15,8 @@ from .paths import DatasetPath
 )
 class RegularTimeseries:
     path: DatasetPath
-    values: np.ndarray
-    dates: list[datetime]
+    values: NDArray[float64]
+    dates: NDArray[datetime64]
     period_type: str
     units: str
     interval: str
@@ -27,51 +26,6 @@ class RegularTimeseries:
 
     def __len__(self) -> int:
         return len(self.values)
-
-    @staticmethod
-    def from_pydsstools(obj: Any, path: str | DatasetPath) -> Self:
-        attr_map = {
-            "values": "values",
-            "dates": "pytimes",
-            "period_type": "type",
-            "units": "units",
-            "interval": "interval",
-        }
-        kwargs = {L: getattr(obj, R) for L, R in attr_map.items()}
-        # Add the path object to the keyword argument dict
-        if isinstance(path, str):
-            path = DatasetPath.from_str(path)
-        kwargs["path"] = path
-        # Replace no-data with nan
-        kwargs["values"][obj.nodata] = np.nan
-        # Adjust the way pydsstools interprets dates in HEC-DSS files.
-        dates = kwargs["dates"]
-        interval = kwargs["interval"]
-        # Only fix for intervals greater gte 1 day
-        if interval >= (60 * 60 * 24):
-            fixed_dates = list()
-            for date in dates:
-                # Midnight in HEC-DSS belongs to the day prior, which differs
-                # from the datetime module. Offset by 1 second to compensate.
-                if date.time() == time(0, 0):
-                    date = date - timedelta(seconds=1)
-                fixed_dates.append(date)
-            kwargs["dates"] = fixed_dates
-
-        return RegularTimeseries(**kwargs)
-
-    def from_pyhecdss(obj: Any, path: str | DatasetPath) -> Self:
-        attr_map = {
-            "period_type": "type",
-            "units": "units",
-        }
-        kwargs = {L: getattr(obj, R) for L, R in attr_map.items()}
-        # Add the path object to the keyword argument dict
-        if isinstance(path, str):
-            path = DatasetPath.from_str(path)
-        kwargs["path"] = path
-        print(obj)
-        return RegularTimeseries(**kwargs)
 
     def to_frame(self) -> DataFrame:
         header = dict(self.path.items())
