673a21d285c15be0d5952e84dc8498b8ea284c45
Zachary Roy <zachary.roy@water.ca.gov>
2024-01-12 10:58:19 -0800

    Detect version from installation metadata

--- pandss/__init__.py
+++ pandss/__init__.py
@@ -3,13 +3,26 @@
 Zachary Roy, DWR
 June 2023
 """
+from importlib.metadata import PackageNotFoundError, version
+
 from . import errors, keywords
 from .catalog import Catalog
 from .dss import DSS
 from .paths import DatasetPath, DatasetPathCollection
 from .timeseries import RegularTimeseries
-from .utils import (copy_multiple_rts, copy_rts, read_catalog,
-                    read_multiple_rts, read_rts)
+from .utils import (
+    copy_multiple_rts,
+    copy_rts,
+    read_catalog,
+    read_multiple_rts,
+    read_rts,
+)
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:
+    # pandss not installed, likely developer mode
+    __version__ = None
 
 __all__ = [
     "DSS",
