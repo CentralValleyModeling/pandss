d2605ad45cbc8de427825546af20109daeca57d5
Zachary Roy <zachary.roy@water.ca.gov>
2024-01-18 12:47:18 -0800

    Bring common units and Quantity object into module

--- pandss/units/__init__.py
+++ pandss/units/__init__.py
@@ -2,3 +2,24 @@ import pint
 
 ureg = pint.UnitRegistry()
 ureg.load_definitions(r".\pandss\units\pandss_units.pint")
+
+# Unpack so the pdss.units.Quantity object belongs to the correct registry
+Quantity = ureg.Quantity
+
+# Unpack common units so they are accessible as pandss.units.XXXX
+# Time
+day = ureg.days
+month = ureg.month
+year = ureg.year
+# Flow
+cfs = ureg.cfs
+# Volume
+taf = ureg.taf
+af = ureg.af
+# Salinity
+umhos = ureg.umhos
+cm = ureg.cm
+# Unitless
+percent = ureg.percent
+undefined = ureg.undefined
+logical = ureg.logical
