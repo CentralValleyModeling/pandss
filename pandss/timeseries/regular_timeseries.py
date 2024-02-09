458fdd73008d69e5a78c59759908d41ebbdcb708
Zachary Roy <zachary.roy@water.ca.gov>
2024-02-09 14:54:16 -0800

    Add `__add__` method to `RegularTimeseries`
    Add the ability to add multiple timeseries together. Performs a left join on the left operands index and adds the
    values together row-wise. Addition will fail if units are not
    compatible at the class level, pint compat will not be
    checked.

--- pandss/timeseries/regular_timeseries.py
+++ pandss/timeseries/regular_timeseries.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, fields
+from typing import Self
 from warnings import catch_warnings
 
 from numpy import datetime64
@@ -50,6 +51,61 @@ class RegularTimeseries:
             elif getattr(self, f.name) != getattr(__other, f.name):
                 return False
         return True
+
+    def __add__(self, __other: Self) -> Self:
+        df, kwargs = self._prepare_for_arithmetic(__other)
+        df = df.sum(axis=1)
+        new_values = Quantity(df.values, kwargs["units"])
+
+        return RegularTimeseries(values=new_values, **kwargs)
+
+    def _prepare_for_arithmetic(self, __other: Self) -> tuple[DataFrame, dict]:
+        # Validate action
+        if not isinstance(__other, self.__class__):
+            raise ValueError(
+                f"Cannot perform arithmatic {self.__class__.__name__} "
+                + f"with {type(__other)}"
+            )
+        if __other.units != self.units:
+            raise ValueError(
+                f"Cannot perform arithmatic between {self.__class__.__name__} with"
+                + f"differing units: {self.units}, {__other.units}"
+            )
+        for attr in ("interval", "period_type"):
+            s = getattr(self, attr)
+            o = getattr(__other, attr)
+            if s != o:
+                raise ValueError(f"Cannot add differing {attr}: {s}, {o}")
+        # Get kwargs for new instance
+        # units
+        if isinstance(self.values, Quantity):
+            new_units = self.values.units
+        else:
+            new_units = self.units
+        # path
+        new_path_kwargs = dict()
+        for part in ("a", "b", "c", "d", "e", "f"):
+            part_self = getattr(self.path, part)
+            part_other = getattr(__other.path, part)
+            if part_self == part_other:
+                new_path_kwargs[part] = part_self
+            else:
+                new_path_kwargs[part] = f"{part_self}+{part_other}"
+        if self.path == __other.path:  # Rare case of adding identical paths
+            new_path_kwargs["b"] = f"{self.path.b}+{__other.path.b}"
+        new_path = DatasetPath(**new_path_kwargs)
+        df_self = self.to_frame()
+        df_other = __other.to_frame()
+        df_combined = df_self.join(df_other, how="left", rsuffix="_OTHER")
+
+        kwargs = dict(
+            path=new_path,
+            dates=df_combined.index.values,
+            units=new_units,
+            period_type=self.period_type,
+            interval=self.interval,
+        )
+        return df_combined, kwargs
 
     def to_frame(self) -> DataFrame:
         header = dict(self.path.items())
