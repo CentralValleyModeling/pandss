d216c4a3dbd4bb2002065f1b949a47f65561ba3c
Zachary Roy <zachary.roy@water.ca.gov>
2024-03-29 13:58:36 -0700

    Use pickle-able offset lookup functions

--- src/pandss/timeseries/interval.py
+++ src/pandss/timeseries/interval.py
@@ -1,25 +1,45 @@
 from functools import total_ordering
 from re import compile
+from typing import Callable
+from warnings import warn
 
 from pandas import offsets
 
+
+class AnnualOffset:
+    """Used to create offset factories for different multi-year offsets not in
+    pandas by default.
+    """
+
+    def __init__(self, multiplier: int):
+        self.multiplier = multiplier
+
+    def __call__(self, n: int) -> offsets.YearEnd:
+        return offsets.YearEnd(n * self.multiplier)
+
+
+class NotImplementedInterval(NotImplementedError):
+    def __init__(self, *args, **kwargs):
+        super().__init__("The interval is not supported.")
+
+
 # values from DSS documentation
 # https://www.hec.usace.army.mil/confluence/dssdocs/dsscprogrammer/time-series-data
-INTERVAL_TO_PANDAS_OFFSET = {
+INTERVAL_TO_PANDAS_OFFSET: dict[str, Callable[[int], offsets.BaseOffset]] = {
     "YEAR": offsets.YearEnd,
     "MON": offsets.MonthEnd,
     "MONTH": offsets.MonthEnd,
     "SEMI-MONTH": offsets.SemiMonthEnd,
     # Not supported in pandas, so raise an error when attempting
-    "TRI-MONTH": lambda n: NotImplementedError("TRI-MONTH interval not supported"),
+    "TRI-MONTH": NotImplementedInterval,
     "WEEK": offsets.Week,
     "DAY": offsets.Day,
     "HOUR": offsets.Hour,
     "MIN": offsets.Minute,
     "MINUTE": offsets.Minute,
     "SECOND": offsets.Second,
-    "IR-CENTURY": lambda n: offsets.YearEnd(n * 100),
-    "IR-DECADE": lambda n: offsets.YearEnd(n * 10),
+    "IR-CENTURY": AnnualOffset(100),
+    "IR-DECADE": AnnualOffset(10),
     "IR-YEAR": offsets.YearEnd,
     "IR-MONTH": offsets.MonthEnd,
     "IR-DAY": offsets.Day,
@@ -53,13 +73,13 @@ class Interval:
             raise ValueError(f"cannot find offset for {e=}")
         if not n:
             n = 1
-        self.offset: offsets.DateOffset = factory(int(n))
+        self.offset = factory(int(n))
         self.e = e
 
     def __eq__(self, __other) -> bool:
         if isinstance(__other, Interval):
-            __other = __other.seconds
-        return self.seconds == __other
+            __other = __other.offset
+        return self.offset == __other
 
     def __lt__(self, __other) -> bool:
         if isinstance(__other, Interval):
@@ -80,13 +100,25 @@ class Interval:
         try:
             return self.offset.nanos / 1_000_000_000
         except ValueError:
-            raise AttributeError(
-                "interval is not always the same length, "
-                + "cannot determine the number of seconds: "
-                + "leap years, 31/30/29/28 day months, etc."
-                + "pandas raises an error, see above."
+            warn(
+                f"\nThis offset type ({self.offset.name}) has irregular "
+                + "durations, so pandas will not calculate the seconds in this"
+                + "period. However, pandss defaults to treating all "
+                + "non-regular width offsets as if they were non-leap year, "
+                + "30 day months.\nThis is consistent with DSS internals as of"
+                + " March 2024.\npandss only supports this for year, month, "
+                + "and week offsets.",
+                category=Warning,
             )
+            seconds_in_offset = {
+                "Y": 365 * 24 * 60 * 60,
+                "M": 30 * 24 * 60 * 60,
+                "W": 7 * 24 * 60 * 60,
+            }
+            key = self.offset.name[0].upper()
+            return seconds_in_offset[key]
 
     @property
     def freq(self) -> str:
+        """alias for Interval.offset"""
         return self.offset
