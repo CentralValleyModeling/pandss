77076a5d0b8cfee308dce78d8b266a88806400aa
Zachary Roy <zachary.roy@water.ca.gov>
2024-04-23 13:05:34 -0700

    Add note to build script

--- build_all.bat
+++ build_all.bat
@@ -2,5 +2,6 @@ SETLOCAL
 CALL conda config --set report_errors false
 CALL conda activate dev_pandss
 CALL conda build . --output-folder .\dist > .\dist\record_conda.txt 2>&1
+rem don't forget to convert this output using `conda convert ...`
 ENDLOCAL
 PAUSE
