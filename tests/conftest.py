71903f99365e11eb86f68285e20264f5b8f120b2
Zachary Roy <zachary.roy@water.ca.gov>
2024-08-19 11:00:14 -0700

    Change testing config to use better temp names

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,8 @@
+import datetime
 import logging
 from os import remove
+from os.path import getmtime
 from pathlib import Path
-from random import choice
-from string import ascii_letters
 from typing import Generator
 
 import pytest
@@ -10,43 +10,60 @@ import pytest
 import pandss as pdss
 
 
-@pytest.fixture()
+def cleanup(d: Path):
+    cutoff = datetime.datetime.now() - datetime.timedelta(minutes=1)
+    if d.is_file():
+        remove(d)
+    else:
+        for f in d.iterdir():
+            if f.is_dir():
+                cleanup(f)
+            elif datetime.datetime.fromtimestamp(getmtime(f)) < cutoff:
+                try:
+                    remove(f)
+                except PermissionError:
+                    logging.warning(f"couldn't clean up old file {f}")
+
+
+@pytest.fixture(scope="session")
 def dss_6() -> Path:
     pdss.module_engine.set("pyhecdss")
     return Path().resolve() / "tests/assets/existing/v6.dss"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def dss_7() -> Path:
     pdss.module_engine.set("pydsstools")
     return Path().resolve() / "tests/assets/existing/v7.dss"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def dss_large() -> Path:
     pdss.module_engine.set("pyhecdss")
     return Path().resolve() / "tests/assets/existing/large_v6.dss"
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def assets() -> Path:
     return Path().resolve() / "tests/assets"
 
 
-@pytest.fixture()
-def created_dir(assets) -> Path:
-    return assets / "created"
+@pytest.fixture(scope="session")
+def created_dir(assets) -> Generator[Path, None, None]:
+    d = Path(assets / "created")
+    yield d
+    cleanup(d)
 
 
 @pytest.fixture(scope="function")
 def random_name() -> Generator[str, None, None]:
-    name = "".join(choice(ascii_letters) for _ in range(10))
-    yield f"{name}.dss"
+    name = datetime.datetime.now().isoformat(timespec="seconds").replace(":", ".")
+    yield name
 
 
 @pytest.fixture(scope="function")
 def temporary_dss(created_dir: Path, random_name: str) -> Generator[Path, None, None]:
-    file = Path(created_dir / random_name)
+    file = Path(created_dir / f"{random_name}.dss")
     yield file
     remove(file)
     for f in file.parent.iterdir():
